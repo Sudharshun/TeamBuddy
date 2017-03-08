3f6baed16eb5b0cadf99ce228b901ee6f5e32fe8
sudharshun@gmail.com <sudharshun@gmail.com>
2017-03-07 23:45:15 -0600

    added data/links.json

---
+++ assets/data/links.js
@@ -0,0 +1,31 @@
+var links= 
+[
+{
+"title":"Google",
+"url":"http://www.google.com"
+},
+{
+"title":"CNN",
+"url":"http://www.cnn.com"
+}
+,
+{
+"title":"News",
+"sublinks":[
+{
+"title":"CNN1",
+"url":"http://www.cnn.com"
+},
+{
+"title":"Fox",
+"url":"http://www.cnn.com"
+},
+{
+"title":"NDTV",
+"url":"http://www.cnn.com"
+}
+]
+}
+]
+
+module.exports= links;
--- renderer-process/usefulinfo/links-processor.js
+++ renderer-process/usefulinfo/links-processor.js
@@ -1,11 +1,33 @@
+const path = require('path')
+const linksNow = require(path.resolve('assets/data/links'))
 window.addEventListener('load', function (event) {
-  console.log('called Links page')
-var txt1 = document.createTextNode(" This text was added to the DIV.");
-var txt2 = document.createTextNode(" This text was also added to the DIV.");
+  console.log('called Links page' + linksNow[0].url)
+  var linksTable = ""
 
-document.getElementById('actualLinks').appendChild(txt1);
-document.getElementById('actualLinks').appendChild(txt2);
+  if (linksNow) {
+    linksTable += "<table border='1' align=\"left\">"
+    for (link in linksNow) {
+      if (linksNow[link].url) {
+        linksTable += "<tr><td>" + linksNow[link].title + "</td><td><a href='" + linksNow[link].url + "'/>" + linksNow[link].url + "</a></td></tr>";
+      }
+      if (linksNow[link].sublinks) {
+        linksTable += "<tr><td>" + linksNow[link].title + "</td><td><table border='1' align='left'>"
+        for (sublink in linksNow[link].sublinks) {
+          linksTable += "<tr><td>" + linksNow[link].sublinks[sublink].title + "</td><td><a href='" + linksNow[link].sublinks[sublink].url + "'/>" + linksNow[link].sublinks[sublink].url + "</a></td></tr>";
+        }
 
+        linksTable += "</table></td></tr>"
+
+      }
+
+    }
+    linksTable += "</table>"
+ 
+
+
+    document.getElementById('actualLinks').innerHTML = linksTable
+
+  }
 
 
 })
